c525b6facc0cb128e1e2705c6e3a87ffa5cd7a07
Hisham Bhatti <hishamb@cs.washington.edu>
2025-12-21 19:35:49 -0800

    Fixed name

--- image-compression-svd-frontend/src/utils/utils.js
+++ image-compression-svd-frontend/src/utils/utils.js
@@ -2,7 +2,7 @@ const base = import.meta.env.BASE_URL.replace(/\/$/, "");
 
 export const portfolioPages = [
   [
-    { name: 'Rattlesnake', folder: 'rattlesnake', path: `${base}/images/rattlesnake/rattlesnake_grayscale.png` },
+    { name: 'Rattlesnake Ledge', folder: 'rattlesnake', path: `${base}/images/rattlesnake/rattlesnake_grayscale.png` },
     { name: 'Heart', folder: 'heart', path: `${base}/images/heart/heart.png` },
     { name: 'Hisham', folder: 'hisham', path: `${base}/images/hisham/hisham.jpg` },
     { name: 'Joshua Tree', folder: 'joshua-tree', path: `${base}/images/joshua-tree/joshua-tree.png` },
